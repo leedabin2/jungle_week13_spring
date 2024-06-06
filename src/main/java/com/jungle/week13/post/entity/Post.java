a3d599a183080af8837a749344ca04884d633015
leedabin2 <12193152@inha.edu>
2024-06-06 15:02:19 +0900

    fix: 필드명 카멜 표기법으로 변경

--- src/main/java/com/jungle/week13/post/entity/Post.java
+++ src/main/java/com/jungle/week13/post/entity/Post.java
@@ -44,11 +44,11 @@ public class Post{
 
     @CreatedDate
     @Column(name = "created_at", nullable = false)
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updated_at;
+    private LocalDateTime updatedAt;
 
 
 
