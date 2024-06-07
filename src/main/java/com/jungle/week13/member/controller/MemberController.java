c3e5811dd60c1c2dbe42e4a8e7a3b6cd3225c0b1
leedabin2 <12193152@inha.edu>
2024-06-07 14:21:27 +0900

    refactor: 회원가입시 유효성 검증 실패할 경우 MethodArgumentNotValidException으로 예외처리

--- src/main/java/com/jungle/week13/member/controller/MemberController.java
+++ src/main/java/com/jungle/week13/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +25,10 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody @Valid AuthRequest addMemberDto, BindingResult bindingResult) {
+    public ResponseEntity<?> signup(@RequestBody @Valid AuthRequest addMemberDto, BindingResult bindingResult) throws MethodArgumentNotValidException {
 
         if (bindingResult.hasErrors()) //유효성 검증 실패 시 에러 메시지 클라이언트에게 전달
-            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+            throw new MethodArgumentNotValidException(null,bindingResult);
 
         try {
             CommonResponse<String> registerResponse = memberService.signup(addMemberDto);
@@ -36,6 +37,5 @@ public class MemberController {
             // 아이디 중복 예외 처리
             return ResponseEntity.badRequest().body(CommonResponse.error(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
         }
-
     }
 }
