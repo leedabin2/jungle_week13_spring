c578a6ef478c9230335cc38b81be2f58ac320c07
leedabin2 <12193152@inha.edu>
2024-06-07 14:49:30 +0900

    fix: 아이디, 비밀번호 정규표현식 수정

--- src/main/java/com/jungle/week13/member/dto/AuthRequest.java
+++ src/main/java/com/jungle/week13/member/dto/AuthRequest.java
@@ -15,11 +15,11 @@ import lombok.NoArgsConstructor;
 public class AuthRequest {
 
     @NotBlank(message = "아이디는 필수 입력 값입니다.")
-    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "아이디는 4~10자의 알파벳 소문자와 숫자로 구성되어야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{4,10}$", message = "아이디는 4~10자의 알파벳 소문자와 숫자로 구성되어야 합니다.")
     private String username;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$", message = "비밀번호는 8~15자의 알파벳 대소문자와 숫자로 구성되어야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,15}$", message = "비밀번호는 8~15자의 알파벳 대소문자와 숫자로 구성되어야 합니다.")
     private String password;
 
 
