203284a06e68f8ac862b7a69f7411ed99a42c3d5
leedabin2 <12193152@inha.edu>
2024-06-07 14:19:14 +0900

    feat: 회원가입 유효성 검증 실패시 global exception 구현 ver1.0

---
+++ src/main/java/com/jungle/week13/exception/GlobalExceptionController.java
@@ -0,0 +1,18 @@
+package com.jungle.week13.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.jungle.week13.member.dto.ErrorResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionController {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(BindingResult bindingResult) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.of(bindingResult));
+
+    }
+}
