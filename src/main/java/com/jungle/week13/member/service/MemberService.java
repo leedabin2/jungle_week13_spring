d6b83ab8dbd3a9e9b790f9ea1c85d781bd133ba0
leedabin2 <12193152@inha.edu>
2024-06-08 19:06:39 +0900

    feat: login jwt header 전송

--- src/main/java/com/jungle/week13/member/service/MemberService.java
+++ src/main/java/com/jungle/week13/member/service/MemberService.java
@@ -2,13 +2,17 @@ package com.jungle.week13.member.service;
 
 import com.jungle.week13.common.dto.CommonResponse;
 import com.jungle.week13.exception.DuplicateMemberException;
+import com.jungle.week13.exception.MemberNotFoundException;
+import com.jungle.week13.jwt.JwtUtil;
 import com.jungle.week13.member.dto.AuthRequest;
 import com.jungle.week13.member.repository.MemberRepository;
 import com.jungle.week13.member.entity.Member;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +25,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public CommonResponse<String> signup(AuthRequest addMemberDto) {
 
@@ -49,11 +54,22 @@ public class MemberService {
 
             return CommonResponse.success("회원 가입 성공","회원가입을 환영합니다.");
         } catch (DataIntegrityViolationException e) { // 데이터 무결성 검사
-            log.error("회원 가입 실패 ",addMemberDto.getUsername(), e.getMessage());
+            log.error("회원 가입 실패",addMemberDto.getUsername(), e.getMessage());
             return CommonResponse.error(HttpStatus.BAD_REQUEST,"회원가입 실패");
         }
+    }
 
+    /* 로그인 로직 메서드 */
+    public void login(AuthRequest authDto, HttpServletResponse response) {
 
+        Member member = memberRepository.findByUsername(authDto.getUsername())
+                .orElseThrow(() ->  new MemberNotFoundException("존재하지 않는 회원입니다."));
+
+        if (!passwordEncoder.matches(authDto.getPassword(),member.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다."); // 스프링 시큐리티를 사용하는 경우에만 발생
+        }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, JwtUtil.createToken(authDto.getUsername()));
     }
 
 }
